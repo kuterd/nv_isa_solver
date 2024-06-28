79cf8b213c375a2e968a5149e53c8e99cdd5b0a0
Kuter Dinel <kuterdinel@gmail.com>
2024-06-29 02:05:19 +0300

    fix populate_cache script

--- populate_cache.py
+++ populate_cache.py
@@ -1,17 +1,27 @@
+from concurrent import futures
+from argparse import ArgumentParser
+
 from disasm_utils import Disassembler, set_bit_range2
 
-disassembler = Disassembler("SM90a")
+
+parser = ArgumentParser()
+parser.add_argument("--arch", default="SM90a")
+parser.add_argument("--cache_file", default="disasm_cache.txt")
+parser.add_argument("--nvdisasm", default="nvdisasm")
+arguments = parser.parse_args()
+
+disassembler = Disassembler(arguments.arch, nvdisasm=arguments.nvdisasm)
 
 
 def flip_bit(array, i):
     bit_offset = i % 8
-    array[i // 8] |= 1 << bit_offset
+    array[i // 8] ^= 1 << bit_offset
 
 
 inst = []
 for i in range(pow(2, 12)):
     # Read write barriers!
-    array = bytearray(bytes.fromhex("20090000000000000000800300c00f00"))
+    array = bytearray(bytes.fromhex("00000000000000000000000000e00f00"))
     set_bit_range2(array, 0, 12, i)
     inst.append(array)
     for j in range(13, 8 * 13):
@@ -19,5 +29,5 @@ for i in range(pow(2, 12)):
         flip_bit(array_, j)
         inst.append(array)
 
-disassembler.disassemble_parallel(inst, "SM90a")
-disassembler.dump_cache("sm90a_cache.txt")
+disassembler.disassemble_parallel(inst, arguments.arch)
+disassembler.dump_cache(arguments.cache_file)
